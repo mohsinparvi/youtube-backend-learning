61bb4d7b1d9837523e9df51b28ef08563c7d8323
Mohsin Hassan <86055307+aboutmohsin@users.noreply.github.com>
2024-01-08 12:12:29 +0500

    connect: establishe DB connection

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+/*
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Our application not able to talk to MongoDB", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`Our application is running at port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error", error);
+    throw error;
+  }
+})();*/
